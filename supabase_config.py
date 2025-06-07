b9f971a803b9f26cd5e13109957e81c855fa1b16
al3p1 <hiamongus9823@gmail.com>
2025-06-07 21:40:34 +0300

    Create supabase_config.py

---
+++ supabase_config.py
@@ -0,0 +1,7 @@
+SUPABASE_URL = "https://nyqjeyruewajkkyohplw.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55cWpleXJ1ZXdhamtreW9ocGx3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkwNTQ5MTQsImV4cCI6MjA2NDYzMDkxNH0.t2C7cEpTSVugFzrV4biLb5VrsrOFFBGwwEc3e929s3g"
+
+HEADERS = {
+    "apikey": SUPABASE_KEY,
+    "Authorization": f"Bearer {SUPABASE_KEY}"
+}
